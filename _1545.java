bfc224435be5caeadc41f42436877c118fba1834
sungwuk <psyy94@naver.com>
2018-07-12 22:00:16 +0900

    SW_Expert
    18.07

---
+++ _1545.java
@@ -0,0 +1,16 @@
+package fg;
+import java.util.*;
+public class _1545 {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		int[] a=new int[n+1];
+		for(int i=0;i<=n;i++) {
+			System.out.print((int)Math.pow(2, i)+" ");
+		}
+		
+	}
+
+}
