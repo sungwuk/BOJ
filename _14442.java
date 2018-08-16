ef5145c5d1ddcebcca561b070b331267f3827bde
sungwook <psyy94@naver.com>
2018-08-16 16:39:39 +0900

    18.08
    18.08

---
+++ _14442.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class _14442 {
+	static int[]dx= {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+	static class Point{
+		int x,y,chance,dep;
+		Point(int x, int y, int d,int c){
+			this.x=x;this.y=y;this.dep=d;this.chance=c;
+		}
+	}
+	public static void main(String[] args) {
+		int n,m,k;
+		Scanner sc= new Scanner(System.in);
+		n=sc.nextInt();m=sc.nextInt();k=sc.nextInt();
+		boolean map[][]= new boolean[n+1][m+1];
+		int dist[][][] = new int[n+1][m+1][11];
+		for(int i=1;i<=n;i++) {
+			String in=sc.next();
+			for(int j=1;j<=m;j++) {
+				if(in.charAt(j-1)==0) map[i][j]=true;
+			}
+		}
+		for(int i=1;i<=n;i++)for(int j=1;j<=m;j++)for(int t=0;t<11;t++)dist[i][j][t]=Integer.MAX_VALUE;
+		Queue<Point> q= new LinkedList<>();
+		q.add(new Point(1,1,1,0));				// k번 부실수 있음.
+		dist[1][1][0]=1;
+		while(!q.isEmpty()) {
+			Point tmp = q.poll();
+			int cx = tmp.x;
+			int cy = tmp.y;
+			int cd =tmp.dep;
+			int cc = tmp.chance;
+			if(cx==n&&cy==m) {
+				System.out.println(cd);
+				return;
+			}
+			for(int i=0;i<4;i++) {
+				int nx =cx+dx[i];
+				int ny = cy+dy[i];
+				if(nx<1||ny<1||nx>n||ny>m)continue;
+				if(!map[nx][ny]&&cc<k) {
+						dist[nx][ny][cc+1]=cd+1;
+						q.add(new Point(nx,ny,cd+1,cc+1));
+				}
+				else {
+					dist[nx][ny][cc]=cd+1;
+					q.add(new Point(nx,ny,cd+1,cc));
+				}
+			
+			}
+		}
+		System.out.println(-1);
+	}
+
+}
