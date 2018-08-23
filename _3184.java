7210215842756d17348d00f3680ef181edd819a9
sungwook <psyy94@naver.com>
2018-08-24 00:04:16 +0900

    18.08
    18.08

--- _3184.java
+++ _3184.java
@@ -2,75 +2,63 @@ import java.util.*;
 public class _3184 {
 	static int[] dx = {1,-1,0,0};
 	static int[] dy = {0,0,1,-1};
+	static boolean [][] check ;
+	static 	char[][] map ;
+	static int r,c,total_wolf=0,total_sheep=0;
 	static class Point{
 		int x, y;
 		Point(int x,int y){
 			this.x=x;this.y=y;
 		}
 	}
+	static void find(int x, int y) {
+		LinkedList<Point> list = new LinkedList<>();
+		list.add(new Point(x,y));
+		check[x][y]=true;
+		int wolf =0,sheep=0;
+		while(!list.isEmpty()) {
+			Point tmp = list.poll();
+			int cx = tmp.x;
+			int cy = tmp.y;
+			if(map[cx][cy]=='v') wolf++;
+			if(map[cx][cy]=='o') sheep++;
+			
+			for(int i=0;i<4;i++) {
+				int nx = cx+dx[i];
+				int ny = cy+dy[i];
+				if(nx<0||ny<0||nx>=r||ny>=c) continue;
+				if(!check[nx][ny]&&map[nx][ny]!='#') {
+					check[nx][ny]=true;
+					list.add(new Point(nx,ny));
+				}
+			}
+		}
+		if(wolf<sheep) total_sheep+=sheep;
+		else total_wolf+=wolf;
+
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int r,c;
 		r=sc.nextInt();c=sc.nextInt();
-		char[][] map = new char[r][c];
-		boolean [][] check = new boolean[r][c];
+		map = new char[r][c];
+		check = new boolean[r][c];
 		LinkedList<Point> list = new LinkedList<>();
 		for(int i=0;i<r;i++) {
 			String in = sc.next();
 			for(int j=0;j<c;j++) {
 				map[i][j]=in.charAt(j);
-				if(map[i][j]=='v') list.add(new Point(i,j));
 			}
 		}
-		LinkedList<Point> tlist = new LinkedList<>();
-		int total_lamb=0;
-		int total_wolf=0;
-		while(!list.isEmpty()) {
-			Point t = list.peek();
-			while(true) {
-				if(t==null) break;
-				if(check[t.x][t.y]==false) {
-					list.poll();
-					break;
-				}
-				t=list.poll();
-			}
-			if(t==null) break;
-			tlist.add(t);
-			int lamb=0;
-			int wolf =1;
-			while(!tlist.isEmpty()) {
-				Point tmp = tlist.poll();
-				int cx = tmp.x;
-				int cy = tmp.y;
-				check[cx][cy]=true;
-				for(int i=0;i<4;i++) {
-					int nx = cx+dx[i];
-					int ny = cy+dy[i];
-					if(nx<0||ny<0||nx>=r||ny>=c) continue;
-					if(map[nx][ny]=='v'&&!check[nx][ny]) {
-						wolf++;
-						check[nx][ny]=true;
-						tlist.add(new Point(nx,ny));
-						continue;
-					}
-					if(map[nx][ny]!='#'&&!check[nx][ny]) {
-						if(map[nx][ny]=='o') {
-							lamb++;
-						}
-						check[nx][ny]=true;
-						tlist.add(new Point(nx,ny));
+		for(int i=0;i<r;i++) {
+			for(int j=0;j<c;j++) {
+				if(map[i][j]=='v'||map[i][j]=='o') {
+					if(!check[i][j]) {
+						find(i,j);
 					}
 				}
 			}
-			if(lamb>wolf) { 
-				total_lamb+=lamb;
-			}
-			else {
-				total_wolf+=wolf;
-			}
 		}
-		System.out.println(total_lamb+" "+total_wolf);
+		System.out.println(total_sheep+" "+total_wolf);
 	}
 
 }
