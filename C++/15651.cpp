efdc5ec5db59f47faf0f94430654f9ffbbfa64d8
sungwook <psyy94@naver.com>
2018-09-19 04:10:55 +0900

    18.09
    18.09

---
+++ C++/15651.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+using namespace std;
+int n, m, arr[8];
+bool chk[8];
+vector<int> v;
+void solve(int k) {
+	if (k == m) {
+	
+		for (int i = 0; i < m; i++) printf("%d ", arr[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+			arr[k] = i + 1;
+			solve(k + 1);
+	}
+}
+int main() {
+	scanf_s("%d %d", &n, &m);
+	solve(0);
+	system("pause");
+}
