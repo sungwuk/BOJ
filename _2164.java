8359dda9f303f5100a7d3a1fbc266a973103da59
sungwook <psyy94@naver.com>
2018-09-26 03:05:35 +0900

    18.09
    18.09

---
+++ _2164.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class _2164 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=1;i<=n;i++) q.add(i);
+		while(q.size()!=1) {
+			q.poll();
+			q.add(q.poll());
+		}
+		System.out.println(q.poll());
+	}
+
+}
