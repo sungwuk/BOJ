15ebd1fe62684a43d516d316bff1dad5726b038b
sungwook <psyy94@naver.com>
2018-10-22 23:16:32 +0900

    18.10
    18.10

---
+++ _1152.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class _1152 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int count=0;
+		while(st.hasMoreTokens()) { 
+		if(st.nextToken()!="")count++;
+		}
+		System.out.println(count);
+	}
+}
